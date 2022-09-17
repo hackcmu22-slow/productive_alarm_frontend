ecd631664d4a9f165251faab918c30ef8ee431a2
tjunjet <latyun142@gmail.com>
2022-09-17 11:22:01 -0400

    Added picture button

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+
+
 const CameraScreen: React.FC = () => {
+    let camera = Camera;
     const [hasPermission, setHasPermission] = useState(null);
     const [type, setType] = useState(CameraType.back);
 
@@ -21,17 +26,31 @@ const CameraScreen: React.FC = () => {
         return <Text>No access to camera</Text>;
     }
 
+    // Taking the picture
+    const __takePicture = async () => {
+        if (!camera) return
+        const photo = await camera.takePictureAsync()
+       
+      }
+
     return (
         <View style={styles.container}>
             <Camera style={styles.camera} type={type} ratio="16:9">
                 <View style={styles.buttonContainer}>
                     <TouchableOpacity
-                        style={styles.button}
+                        style={styles.flipButton}
                         onPress={() => {
                             setType(type === CameraType.back ? CameraType.front : CameraType.back);
                         }}>
+                        
                         <Text style={styles.text}> Flip </Text>
                     </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setType(type === CameraType.back ? CameraType.front : CameraType.back);
+                        }}
+                        style={styles.snapButton}
+                    />
                 </View>
             </Camera>
         </View>
@@ -51,10 +70,31 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         margin: 20,
     },
-    button: {
-        flex: 0.1,
+    flipButton: {
+        flex: 0.25,
+        height: 30,
+        width: 40,
+        textAlign: "center",
+        justifyContent: "center",
+        marginLeft: "5%",
+        marginBottom: "5%",
         alignSelf: 'flex-end',
         alignItems: 'center',
+        backgroundColor: 'grey',
+        borderRadius: 50,
+    },
+    snapButton: {
+        display: 'flex',
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: "20%",
+        marginBottom: "5%",
+        width: 70,
+        height: 70,
+        bottom: 0,
+        borderRadius: 50,
+        backgroundColor: '#fff'
     },
     text: {
         fontSize: 18,
