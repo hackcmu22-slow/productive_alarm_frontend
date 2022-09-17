2b60c5d010854708081126e107c25572b6290e6c
Lhwhatever <llinhong.2301+github@gmail.com>
2022-09-17 01:04:01 -0400

    Add SafeAreaView for iOS users

--- src/screens/AlarmList.tsx
+++ src/screens/AlarmList.tsx
@@ -1,28 +1,34 @@
 import * as React from "react";
-import { ScrollView } from "react-native";
-import { Card, Switch, Text } from "react-native-paper";
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import AlarmListing, { AlarmListingProps } from "../components/AlarmListing";
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 const AlarmList: React.FC = () => {
   const [isAlarmOn, setIsAlarmOn] = React.useState(false);
 
-    const alarms: AlarmListingProps[] = [
-        {
-            name: "hello",
-            hour: 7,
-            minute: 8,
-            enabled: isAlarmOn,
-            onEnabledChange: setIsAlarmOn
-        }
-    ]
+  const alarms: AlarmListingProps[] = [
+    {
+      name: "hello",
+      hour: 7,
+      minute: 8,
+      enabled: isAlarmOn,
+      onEnabledChange: setIsAlarmOn,
+    },
+  ];
 
   return (
-    <ScrollView>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
         {alarms.map((alarm, index) => (
-            <AlarmListing key={index} {...alarm} />
+          <AlarmListing key={index} {...alarm} />
         ))}
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- src/screens/TemplateCopyMe.tsx
+++ src/screens/TemplateCopyMe.tsx
@@ -1,11 +1,19 @@
-import { ScrollView } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import { Text } from "react-native-paper";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
 
 const TemplateCopyMe: React.FC = () => {
   return (
+        <SafeAreaView style={styles.container}>
     <ScrollView>
       <Text>Hello hello</Text>
     </ScrollView>
+    </SafeAreaView>
   );
 };
 
