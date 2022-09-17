40967a66c93f1630885a765f34c36927c26dd89a
Lhwhatever <llinhong.2301+github@gmail.com>
2022-09-17 01:05:49 -0400

    Oops, missed out

--- src/screens/TemplateCopyMe.tsx
+++ src/screens/TemplateCopyMe.tsx
@@ -2,17 +2,17 @@ import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import { Text } from "react-native-paper";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
-})
+  container: {
+    flex: 1,
+  },
+});
 
 const TemplateCopyMe: React.FC = () => {
   return (
-        <SafeAreaView style={styles.container}>
-    <ScrollView>
-      <Text>Hello hello</Text>
-    </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Text>Hello hello</Text>
+      </ScrollView>
     </SafeAreaView>
   );
 };
