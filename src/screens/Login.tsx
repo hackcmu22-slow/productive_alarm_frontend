da1d60e0550024e2bb20c38b0d7f83b7118bf055
tjunjet <latyun142@gmail.com>
2022-09-16 22:42:59 -0400

    Added Damian's Face

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,11 +1,34 @@
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React from 'react';
+import { useState } from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+// import CustomButton from "../components/CustomButton";
+// import CustomTextInput from "../components/CustomInputText";
+// import CustomSnackBar from "../components/CustomSnackBar";
+import { ScrollView } from "../../components/Themed";
+// import Colors from "../constants/Colors";
+// import { firebaseAuth } from "../firebase/firebase";
+// import useColorScheme from "../hooks/useColorScheme";
 
 const Login: React.FC = () => {
   return (
-    <View>
-      <Text>test</Text>
-    </View>
+    <ScrollView style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          padding: 40,
+          marginTop: "10%",
+        }}
+      >
+        <Image
+          style={{ height: 260, width: 260 }}
+          source={require("../../assets/images/damian.png")}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
