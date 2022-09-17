dcda93fb8221f4f458232c4b1564dccee40e2a46
Meng87 <mroupurplax@gmail.com>
2022-09-17 02:09:59 -0400

    Add Navigation page for debugging

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import Login from "./src/screens/Login";
 import Register from "./src/screens/RegisterScreen";
 import AlarmList from "./src/screens/AlarmList";
 import TemplateCopyMe from "./src/screens/TemplateCopyMe";
+import Navigation from "./src/screens/Navigation";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,7 +13,8 @@ export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator initialRouteName="Navigation">
+                <Stack.Screen name="Navigation" component={Navigation} options={{ title: "Navigation"}}/>
                 <Stack.Screen name="Login" component={Login} options={{ title: "Login"}}/>
                 <Stack.Screen name="RegisterScreen" component={Register} options={{ title: "Register"}}/>
                 <Stack.Screen name="Alarm" component={AlarmList} options={{ title: "Alarm"}}/>
